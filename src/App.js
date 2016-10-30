a21bd79b6ef4bb373a5ac47e47bd0ff7da8784ec
Nathan Venn <natevenn@gmail.com>
2016-10-29 20:19:39 -0600

    upload image to fb storage, send image url to fb database, add event listner to sets state to image url and load in img tag

--- src/App.js
+++ src/App.js
@@ -1,23 +1,40 @@
 import React, { Component } from 'react';
 import Header from './components/Header';
 import Picture from './components/Picture';
-import storage from './firebase';
+import fb from './firebase';
 import Dropzone from 'react-dropzone';
 import './styles/App.css';
 
-const storageRef = storage.ref()
+const storageRef = fb.storage().ref();
+const database = fb.database();
 
 class App extends Component {
+  constructor() {
+    super()
+
+    this.state = {
+      imageUrl: ''
+    }
+  }
+
   handleSubmit(e) {
     e.preventDefault()
     var file = this.imageUpload.files[0]
+    var group = 'people'
     var imageRef = storageRef.child(file.name)
+    var dbRef = database.ref('pictures/' + group )
     imageRef.put(file).then(function(snapshot) {
-    var url = snapshot.a.downloadURLs[0]
-    });
-    console.log('file', file)
+      var url = snapshot.a.downloadURLs[0]
+      dbRef.set({
+        imageUrl: url
+      })
+    })
+    dbRef.once('value', (snapshot) => {
+      if(snapshot.val() !== null) {
+        this.setState({ imageUrl: snapshot.val().imageUrl })
+      }
+    })
   }
-
 
   render() {
     return (
@@ -26,6 +43,7 @@ class App extends Component {
         <form>
           <input ref={(ref) => this.imageUpload = ref} type='file' name='image-file' />
           <input onClick={this.handleSubmit.bind(this)} type='submit' />
+          <img src={this.state.imageUrl} width='179' height='280' alt="" />
         </form>
       </div>
     );
